ce351b3171dced214de7df6bdacabee749a39343
Mathieu Steele <matt@steeleagency.com>
2021-09-20 08:10:50 -0400

    use express

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-const http = require("http");
+const express = require("express");
 
-const routes = require("./routes");
+const app = express();
 
-const server = http.createServer(routes);
+// app.use((req, res, next) => {
+//   console.log("In the middleware!");
+//   next(); // Allow the request to go to next middleware
+// });
 
-server.listen(3000);
+app.use("/add-product", (req, res, next) => {
+  console.log("In the add product page!");
+  res.send("<h1>Hi from product page</h1>");
+});
+
+app.use("/", (req, res, next) => {
+  console.log("In another middleware!");
+  res.send("<h1>Hi from Express</h1>");
+});
+
+app.listen(3000);
